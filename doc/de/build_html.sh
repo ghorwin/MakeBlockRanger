526455e590f74511debf552300f16d26d2235ba4
Andreas Nicolai <A.Nicolai@uit-gmbh.de>
2025-02-16 09:12:46 +0100

    Preparing for github pages.

--- doc/de/build_html.sh
+++ doc/de/build_html.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-ADOC=VICUS-Handbuch
-
 #python3 adoc_utils/adoc-link-check.py . &&
 
 echo '*** Generating html ***' &&
 #python3 adoc_utils/adoc-image-prep.py html . &&
-asciidoctor -a lang=de -a icons=font -a stylesdir=../css -a iconfont-remote!  $ADOC.adoc &&
+asciidoctor -a lang=de -a icons=font -a stylesdir=../css -a iconfont-remote!  main.adoc -o index.html &&
 
 if [ ! -d ../webpage/de/images ]; then
   echo '*** Creating directory ../webpage/de/images' && 
